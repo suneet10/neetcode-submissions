31d444f49e668e1e50c03a10ecb0e547ce962e87
Suneet Jain <44058062+suneet10@users.noreply.github.com>
2026-09-25 15:31:44 -0400

    Add: divide-array-into-equal-pairs - submission-0

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        
+        dic = {}
+
+        for i in nums:
+
+            if i not in dic:
+                dic[i] = 0
+            dic[i] += 1
+
+        for i in dic.values():
+
+            if i%2 !=0:
+                return False
+        return True
